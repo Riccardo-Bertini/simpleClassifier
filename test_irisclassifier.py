984d021f0831e7c1c58a5bcff8b84fb2f1e91c1d
Riccardo Bertini <m_bertini@hotmail.com>
2020-10-30 11:07:22 +0100

    Initial commit

---
+++ test_irisclassifier.py
@@ -0,0 +1,23 @@
+from typing import Set
+import irisclassifier
+import pytest
+
+performance_thresholds = {0.50, 0.75, 0.95}
+
+def test_evaluation():
+ i = irisclassifier.IrisClassifier()
+ i.ingestion()
+ i.segregation()
+ i.train()
+ res = i.evaluation()
+ assert res > 0.75
+
+@pytest.mark.parametrize('th', performance_thresholds)
+def test_evaluations(th):
+ i = irisclassifier.IrisClassifier()
+ i.ingestion()
+ i.segregation()
+ i.train()
+ res = i.evaluation()
+ assert res > th
+
